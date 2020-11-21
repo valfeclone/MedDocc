5c9dca95b6b2e0c26f139b6d074d9320cb16ea3b
Diaz S. Yardha <icehope12@gmail.com>
2020-11-21 21:16:54 +0700

    update database

---
+++ database/migrations/2020_11_21_133629_create_invoices_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+			$table->enum('status',['on_hold','accepted','done','cancelled']);
+			$table->foreignId('user_id')->references('id')->on('users');
+			$table->foreignId('doctor_id')->references('id')->on('doctors');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoices');
+    }
+}
