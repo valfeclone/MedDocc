3058f720a9d979985c454ed337bc3f66f2ea42f2
Diaz S. Yardha <icehope12@gmail.com>
2020-11-23 00:14:18 +0700

    buat login doctor

---
+++ resources/views/doctor/login.blade.php
@@ -0,0 +1,41 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- CSS -->
+    <link href="{{ asset('/css/home_style.css') }}" rel="stylesheet">
+
+    <title>MEDDOC</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <span class="hamburger-icon" id="hamburger-icon-id">
+                <i class="fa fa-bars"></i>
+            </span>
+            <a href="#" class="logo-company"><em><strong>MEDDOC</strong></em></a>
+            <ul class="main-nav" id="main-nav-id">
+                <li>
+                    <a href="#about-us" class="nav-links" id="about-click">About Us</a>
+                </li>
+                <li>
+                    <a href="#meet-guide-id" class="nav-links" id="meet-click">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+	<content>
+		<form>
+			<label for="uname">username</label><br>
+			<input type="text" id="uname" name="uname"><br>
+			<label for="password">Last name:</label><br>
+			<input type="text" id="password" name="password">
+		</form>
+	</content>
+</body>
+</html>
