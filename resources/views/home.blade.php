51cef2e39afd9e38cd52eb9c03b73dc8b0fb4461
dharmatriyasa <dharmatriyasa19@gmail.com>
2020-11-23 00:00:14 +0800

    menambahkan views home.blade.php serta css file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,39 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- CSS -->
+    <link href="{{ asset('/css/home_style.css') }}" rel="stylesheet">
+
+    <title>MEDDOC</title>
+</head>
+<body>
+    <header>
+        <nav class="navbar">
+            <span class="hamburger-icon" id="hamburger-icon-id">
+                <i class="fa fa-bars"></i>
+            </span>
+            <a href="#" class="logo-company"><em><strong>MEDDOC</strong></em></a>
+            <ul class="main-nav" id="main-nav-id">
+                <li>
+                    <a href="#about-us" class="nav-links" id="about-click">About Us</a>
+                </li>
+                <li>
+                    <a href="#meet-guide-id" class="nav-links" id="meet-click">Contact</a>
+                </li>
+                <li>
+                    <a href="#contact-us"class="nav-links" id="contact-click">Login</a>
+                </li>
+                <li>
+                    <a href="#contact-us"class="nav-links" id="contact-click">Register</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
