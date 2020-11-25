632c61c35cd3ce261ca0e316ae9d0fa10bf096a9
dharmatriyasa <dharmatriyasa19@gmail.com>
2020-11-25 15:58:20 +0800

    menambahkan footer pada halaman home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -54,5 +54,85 @@
         </div>
     </header>
 
+    <footer>
+        <div class="container-header-flex" id="contact-us">
+            <div class="footer-about-contact">
+                <div class="flex-row">
+                    <div class="card-section">
+                        <h3 class="white-color logo-company"><em><strong>MEDDOC</strong></em></h3>
+                        <br>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                            Porro magni blanditiis quos. Maxime et quis cupiditate temporibus 
+                            non nemo aspernatur beatae porro aut laudantium. Placeat maiores 
+                            quod quaerat voluptatem iure?
+                        </p>
+                    </div>
+                </div>
+                <div class="flex-row">
+                    <div class="card-section">
+                        <h3 class="white-color logo-company">Contact</h3>
+                        <br>
+                        <p>Phone number contact:</p>
+                        <p><strong>+621916179313</strong></p>
+                        <br>
+                        <p>E-mail contact:</p>
+                        <p><strong>info@selfimprovement.com</strong></p>
+                        <h3 class="white-color logo-company">Address</h3>
+                        <br>
+                        <p><strong>Jalan Pulau Maluku 3 No. 199</strong></p>
+                        <p><strong>Denpasar</strong></p>
+                        <p><strong>Bali</strong></p>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-product-sosmed">
+                <div class="flex-row">
+                    <div class="card-section">
+                        <h3 class="white-color logo-company">Our Value</h3>
+                        <br>
+                        <ul class="product-list">
+                            <li>
+                                <a href="#">Practical</a>
+                            </li>
+                            <li>
+                                <a href="#">Healthy </a>
+                            </li>
+                            <li>
+                                <a href="#">Simple</a>
+                            </li>
+                            <li>
+                                <a href="#">Fastest</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="flex-row">
+                    <div class="card-section">
+                        <h3 class="white-color logo-company">Social Media</h3>
+                        <div class="footer-icon">
+                            <a href="#" class="footers-icon"><i class="fa fa-facebook"></i> MEDDOC ID</a>
+                            <a href="#" class="footers-icon"><i class="fa fa-twitter"></i> @meddoc.id</a>
+                            <a href="#" class="footers-icon"><i class="fa fa-instagram"></i> @meddoc.id</a>
+                            <a href="#" class="footers-icon"><i class="fa fa-linkedin"></i> MEDDOC ID</a>
+                        </div>
+                        <div class="footer-icon-mobile">
+                            <a href="#" class="footers-icon"><i class="fa fa-facebook"></i></a>
+                            <a href="#" class="footers-icon"><i class="fa fa-twitter"></i></a>
+                            <a href="#" class="footers-icon"><i class="fa fa-instagram"></i></a>
+                            <a href="#" class="footers-icon"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                     
+                </div>
+            </div>
+        </div>
+        <div class="bottom-footer">
+            <div class="border-top">
+                <p>Copyright 2020 MEDDOC</p>
+            </div>
+        </div> 
+    </footer>
+
 </body>
 </html>
